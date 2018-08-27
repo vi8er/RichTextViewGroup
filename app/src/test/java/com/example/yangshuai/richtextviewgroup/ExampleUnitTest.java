c262e297a474327470bf97a37e4f7ae927aa4d26
yangshuai <yangshuai01@huatu.com>
2018-08-27 16:56:44 +0800

    init project

---
+++ app/src/test/java/com/example/yangshuai/richtextviewgroup/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.yangshuai.richtextviewgroup;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
